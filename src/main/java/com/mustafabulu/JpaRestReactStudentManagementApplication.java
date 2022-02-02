65f250b7b2ff5fb26ed689aa85315ff682f564b0
mustafabulu <you@example.com>
2022-02-02 07:47:03 +0300

    Initial commit

---
+++ src/main/java/com/mustafabulu/JpaRestReactStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mustafabulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRestReactStudentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRestReactStudentManagementApplication.class, args);
+    }
+
+}
