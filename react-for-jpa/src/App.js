79bd5b3e7e04bab1906ab48b580756d6ea5813de
mustafabulu <you@example.com>
2022-02-03 02:27:42 +0300

    configuration for new component

--- react-for-jpa/src/App.js
+++ react-for-jpa/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ListStudentComponent from './components/ListStudentComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListStudentComponent/>
+    <div>
+      <HeaderComponent/>
+        <div className="container">
+         <ListStudentComponent/>
+        </div>
+      <FooterComponent/>
     </div>
   );
 }
