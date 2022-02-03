f8bb5168bae7aa33d380539832870515addaba8b
mustafabulu <you@example.com>
2022-02-03 03:20:54 +0300

    Configuring Routing install react-router-dom-v6

--- react-for-jpa/src/App.js
+++ react-for-jpa/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import ListStudentComponent from './components/ListStudentComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -8,13 +8,24 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-        <div className="container">
-         <ListStudentComponent/>
-        </div>
-      <FooterComponent/>
+      <Router>
+        <HeaderComponent />
+          <div className='container'>
+              <div className="container">
+                  <Routes>
+                      <Route path = "/" element = {<ListStudentComponent />}> </Route>
+                      <Route path = "/students" element = {<ListStudentComponent />}> </Route>
+                      
+                  </Routes>                      
+              </div>   
+          </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
