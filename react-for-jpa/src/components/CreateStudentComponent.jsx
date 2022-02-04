92300b617a1302d47988e3e283152d942dacf62a
mustafabulu <you@example.com>
2022-02-04 06:02:23 +0300

    component for creating students

---
+++ react-for-jpa/src/components/CreateStudentComponent.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class CreateStudentComponent extends Component {
+    constructor(props){
+
+        super(props)
+
+        this.state={
+
+        }
+    }
+    
+    
+    
+    
+    render() {
+        return (
+            <div>
+                <h1>Ogrenci Ekleme Ekranı</h1>              
+            </div>
+        );
+    }
+}
+
+export default CreateStudentComponent;
